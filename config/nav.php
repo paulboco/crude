31bab467a7ddcc6e0a99087e5a18672c3ae1729b
Paul Boco <paulboco@cableone.net>
2011-08-05 16:04:52 -0500

    Document nav.php config file

--- config/nav.php
+++ config/nav.php
@@ -1,7 +1,38 @@
 <?php
 
+/**
+ * Nav configuration file
+ *
+ * The array is structured as follows:
+ *
+ *     array(
+ *         'GROUP' => array(
+ *             'current_class' => 'CLASSNAME',
+ *             'items' => array(
+ *                 'LABEL' => array(
+ *                     'url' => 'URL',
+ *                     'triggers' => array(
+ *                         'URL2',
+ *                         'URL3',
+ *                           .
+ *                           .
+ *                           .
+ *                     ),
+ *                 ),
+ *             ),
+ *         ),
+ *     )
+ *
+ *    GROUP     - Group to which the nav array belongs.
+ *    CLASSNAME - The CSS class for active items.
+ *    LABEL     - Label of this link.
+ *    URL       - The URL of the link.
+ *    URL2      - URL that triggers a link to be active. * for wildcard
+ *    URL3      - Another URL that triggers a link to be active. * for wildcard
+ */
 
 return array(
+	// the main menu
 	'public' => array(
 		'current_class' => 'current',
 		'items' => array(
@@ -18,48 +49,4 @@ return array(
 			),
 		),
 	),
-	'dashboard' => array(
-		'current_class' => 'current',
-		'items' => array(
-			'Paths' => array(
-				'url' => 'crude/dashboard/fuelinfo/paths',
-				'triggers' => array(
-					'crude',
-					'crude/dashboard/fuelinfo/paths',
-				),
-			),
-			'Routes' => array(
-				'url' => 'crude/dashboard/fuelinfo/routes',
-				'triggers' => array('crude/dashboard/fuelinfo/routes'),
-			),
-			'Request' => array(
-				'url' => 'crude/dashboard/fuelinfo/request',
-				'triggers' => array('crude/dashboard/fuelinfo/request'),
-			),
-			'Modules' => array(
-				'url' => 'crude/dashboard/fuelinfo/modules',
-				'triggers' => array('crude/dashboard/fuelinfo/modules'),
-			),
-			'Packages' => array(
-				'url' => 'crude/dashboard/fuelinfo/packages',
-				'triggers' => array('crude/dashboard/fuelinfo/packages'),
-			),
-			'Database' => array(
-				'url' => 'crude/dashboard/fuelinfo/database',
-				'triggers' => array('crude/dashboard/fuelinfo/database'),
-			),
-			'Session' => array(
-				'url' => 'crude/dashboard/fuelinfo/session',
-				'triggers' => array('crude/dashboard/fuelinfo/session'),
-			),
-			'Config' => array(
-				'url' => 'crude/dashboard/fuelinfo/config',
-				'triggers' => array('crude/dashboard/fuelinfo/config'),
-			),
-			'phpinfo' => array(
-				'url' => 'crude/dashboard/phpinfo',
-				'triggers' => array('crude/dashboard/phpinfo'),
-			),
-		),
-	)
 );
