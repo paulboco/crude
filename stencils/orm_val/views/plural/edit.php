a016dff33b833b48f659ec2a9753eee6b46bb7f1
Paul Boco <paulboco@cableone.net>
2011-08-28 11:34:08 -0500

    Remove stencil 'orm_val' as it doesn't work.

--- stencils/orm_val/views/plural/edit.php
+++
@@ -1,7 +0,0 @@
-<h2>Edit <?php echo $TBL_UCSINGULAR; ?></h2>
-
-<?php echo '<?php'; ?> echo Html::anchor('<?php echo $TBL_PLURAL; ?>/index', 'Listing'); <?php echo '?>'.PHP_EOL; ?>
- |
-<?php echo '<?php'; ?> echo Html::anchor('<?php echo $TBL_PLURAL; ?>/view/'.$<?php echo $TBL_SINGULAR; ?>-><?php echo $TBL_PK; ?>, 'View'); <?php echo '?>'.PHP_EOL; ?>
-
-<?php echo '<?php'; ?> echo render('<?php echo $TBL_PLURAL; ?>/_form'); <?php echo '?>'.PHP_EOL; ?>
